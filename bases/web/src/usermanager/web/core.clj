fc54ee7057002abe6b56561cc99926b67b9c9bca
stoating <zack.slade@gmail.com>
2024-05-02 16:22:57 +0000

    add integrant

--- bases/web/src/usermanager/web/core.clj
+++ bases/web/src/usermanager/web/core.clj
@@ -1,5 +1,43 @@
 (ns usermanager.web.core
+  (:require [integrant.core :as ig]
+            [ring.adapter.jetty :as jetty]
+            [ring.util.response :as resp])
   (:gen-class))
 
+
+;; initialized recursively
+;; handler/greet -> adapter/jetty
+(def config
+  {:adapter/jetty {:port 8080
+                   :handler (ig/ref :handler/greet)}
+   :handler/greet {:name "Alice"}})
+
+
+(defmethod ig/init-key :adapter/jetty
+  [_ {:keys [handler] :as opts}]
+  (jetty/run-jetty handler (-> opts (dissoc :handler) (assoc :join? false))))
+
+
+(defmethod ig/init-key :handler/greet
+  [_ {:keys [name]}]
+  (fn [_] (resp/response (str "Hello " name))))
+
+
+(defmethod ig/halt-key! :adapter/jetty [_ server]
+  (.stop server))
+
+
+(def system
+  (ig/init config))
+
+
 (defn -main []
-  (println "Hello, World!"))
+  system)
+
+(comment
+  ;; shutdown system
+  (ig/halt! system)
+
+  ;; suspend system
+  (ig/suspend! system)
+  )
--- readme.md
+++ readme.md
@@ -11,7 +11,12 @@
 
 # notes
 
-- build with:
-- ``cd projects/usermanager && clojure -T:build uber``
-- run with:
-- ``java -jar projects/usermanager/target/usermanager-standalone.jar``
+- standard
+  - run with:
+  - ``clojure -M:dev -m usermanager.web.core``
+
+- standalone:
+  - build with:
+  - ``cd projects/usermanager && clojure -T:build uber``
+  - run with:
+  - ``java -jar projects/usermanager/target/usermanager-standalone.jar``
