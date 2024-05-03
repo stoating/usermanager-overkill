e1013636a5f31642a5b38518a35366f149f58645
stoating <zack.slade@gmail.com>
2024-05-03 20:17:45 +0000

    initial add of reitit

--- bases/web/src/usermanager/web/core.clj
+++ bases/web/src/usermanager/web/core.clj
@@ -2,7 +2,10 @@
   (:require [clojure.java.io :as io]
             [integrant.core :as ig]
             [nextjournal.beholder :as bh]
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.parameters :as parameters]
             [ring.adapter.jetty :as jetty]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.util.response :as resp]
             [usermanager.filewatcher.interface :as fw]
             [web.home :as home])
@@ -36,7 +39,47 @@
     (jetty/run-jetty handler options)))
 
 
-(defmethod ig/init-key :app/home
+(defn default [req]
+  (assoc-in req [:params :message]
+            (str "Welcome to the User Manager application demo! "
+                 "This uses just Reitit, Ring, and Selmer.")))
+
+(defn render-page
+  []
+  (-> (resp/response (str home/layout))
+      (resp/content-type "text/html")))
+
+
+(defn my-middleware
+  [handler]
+  (fn [req]
+    (let [resp (handler req)]
+      (if (resp/response? resp)
+        resp
+        (render-page)))))
+
+
+(defn app []
+  (ring/ring-handler
+   (ring/router
+    [["/" {:handler default}]]
+    {:data {:middleware [my-middleware
+                         parameters/parameters-middleware
+                         wrap-keyword-params]}})
+   (ring/routes
+    (ring/create-resource-handler
+     {:path "/"})
+    (ring/create-default-handler
+     {:not-found (constantly {:status 404 :body "Not found"})}))))
+
+
+(defmethod ig/init-key :app/run-app
+  [key value]
+  (println "starting:" key)
+  (println "using   :" value)
+  (app))
+
+#_(defmethod ig/init-key :app/home
   [key value]
   (let [name (get value :name)]
     (println "starting:" key)
@@ -70,5 +113,4 @@
 
   (ig/suspend! system)
 
-  (ig/resume config system)
-  )
+  (ig/resume config system))
