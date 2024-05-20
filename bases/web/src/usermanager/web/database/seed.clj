008616941891fb5cd8e3e06a29c9f24d062bb950
stoating <zack.slade@gmail.com>
2024-05-20 19:52:22 +0000

    move seed data to edn files

--- bases/web/src/usermanager/web/database/seed.clj
+++ bases/web/src/usermanager/web/database/seed.clj
@@ -1,37 +1,37 @@
 (ns usermanager.web.database.seed
-  (:require [next.jdbc.sql :as sql]
+  (:require [clojure.edn :as edn]
+            [next.jdbc.sql :as sql]
             [portal.api :as p]
             [xtdb.api :as xt]))
 
 (def departments-seed
-  ["Accounting"
-   "Sales"
-   "Support"
-   "Development"])
+  (-> "./bases/web/resources/database/seed/departments.edn"
+      slurp
+      edn/read-string))
 
-(def user-data-seed
-  [{:first_name "Sean"
-    :last_name "Corfield"
-    :email "sean@worldsingles.com"
-    :department_id 4}])
+(def users-seed
+  (-> "./bases/web/resources/database/seed/users.edn"
+      slurp
+      edn/read-string))
+
 
 (defn seed
   [db]
   (when-not (seq (try
-                   (xt/q db '(from :department [*]))
+                   (xt/q db '(from :departments [*]))
                    (catch Exception _)))
     (tap> db)
     (try
       (doseq [[ix d] (map-indexed vector departments-seed)]
-        (sql/insert! db :department {:name d :xt$id (inc ix)}))
-      (doseq [a user-data-seed]
-        (sql/insert! db :addressbook (assoc a :xt$id (random-uuid))))
+        (sql/insert! db :departments {:name d :xt$id (inc ix)}))
+      (doseq [a users-seed]
+        (sql/insert! db :users (assoc a :xt$id (random-uuid))))
       (println "Populated database with initial data!")
       (catch Exception e
         (println "Exception:" (ex-message e))
         (println "Unable to populate the initial data -- proceed with caution!")))
-    (tap> (xt/q db '(from :addressbook [*])))
-    (tap> (xt/q db '(from :department [*])))))
+    (tap> (xt/q db '(from :users [*])))
+    (tap> (xt/q db '(from :departments [*])))))
 
 (defonce portal (p/open))
 (add-tap #'p/submit)
